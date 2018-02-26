076203d555f64bd9462b2f66fcad4612091d7b42
zcaliptium <zcaliptium@gmail.com>
2018-02-26 15:38:13 +0300

    Engine: Fix the GameMP include paths.

--- Sources/Engine/Query/LegacyMasterServer.cpp
+++ Sources/Engine/Query/LegacyMasterServer.cpp
@@ -24,7 +24,7 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 #include <Engine/Network/Server.h>
 #include <Engine/Network/Network.h>
 #include <Engine/Network/SessionState.h>
-#include <GameMP/SessionProperties.h> // TODO: GET RID OF THIS!
+#include <Game/SessionProperties.h> // TODO: GET RID OF THIS!
 
 #include <Engine/Query/MasterServerMgr.h>
 #include <Engine/Query/MSLegacy.h>
